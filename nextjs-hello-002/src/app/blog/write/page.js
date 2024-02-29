d6cc72412f8a356ef8340d5f170adbdc148120b8
loo919239 <loo919239@naver.com>
2024-02-29 15:01:52 +0900

    next_blog

---
+++ nextjs-hello-002/src/app/blog/write/page.js
@@ -0,0 +1,44 @@
+import styles from "./Write.module.css";
+import { redirect } from "next/navigation";
+
+const FORM_NAME = {
+  CATEGORY: "category",
+  SUBJEC: "subject",
+  CONTENT: "content",
+};
+
+export default () => {
+  // form 의 input 에 입력한 값들을 함수에 전달해주는 매개변수가 필요
+  // formData 에 input 에 입력한 값들이 담겨서 전달된다
+  const actionHandler = async (formData) => {
+    "use server"; // 이 함수의 동작은 마치 서버(back-end) 의 요소처럼 동작하라
+    const data = {
+      category: formData.get(FORM_NAME.CATEGORY),
+      subject: formData.get(FORM_NAME.SUBJEC),
+      content: formData.get(FORM_NAME.CONTENT),
+    };
+    console.log("FORM", data);
+    redirect("/blog/all");
+  };
+  console.log("여기는 어디?");
+  return (
+    <form
+      className={styles.form}
+      action={actionHandler}
+      method="POST"
+    >
+      <input placeholder="카테고리" name={FORM_NAME.CATEGORY} />
+      <input placeholder="제목" name={FORM_NAME.SUBJEC} />
+      <textarea
+        placeholder="내용"
+        rows="20"
+        name={FORM_NAME.CONTENT}
+      />
+      <input
+        className={styles.submit}
+        type="submit"
+        value="저장하기"
+      />
+    </form>
+  );
+};
---
+++ nextjs-hello-002/src/comps/AsideList.js
@@ -0,0 +1,30 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+// import "@/css/AsideList.css";
+import styles from "./AsideList.module.css";
+
+export default () => {
+  // Link, router 를 통해서 열렸을 때 사용한 UL
+  const pathname = usePathname();
+  const routes = [
+    { path: "/blog/write", title: "글쓰기" },
+    { path: "/blog/all", title: "전체글 보기" },
+    { path: "/blog/new", title: "새글 보기" },
+    { path: "/blog/like", title: "인기글 보기" },
+  ];
+  const viewRoutes = routes.map((route) => {
+    return (
+      <li>
+        <Link
+          href={route.path}
+          className={pathname === route.path ? styles.active : ""}
+        >
+          {route.title}
+        </Link>
+      </li>
+    );
+  });
+
+  return <ul className={styles.ul}>{viewRoutes} </ul>;
+};
