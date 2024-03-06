bd2154d8fe388e75b7ebe2126502d791e6546c4a
loo919239 <loo919239@naver.com>
2024-03-06 16:41:41 +0900

    book

--- nextjs-book- 102/src/api/naver_api.js
+++ nextjs-book- 102/src/api/naver_api.js
@@ -1,3 +1,4 @@
+"use server";
 // const NAVER_CLIENT_ID = process.env.NAVER_CLIENT_ID;
 // const NAVER_CLIENT_SECRET = process.env.NAVER_CLIENT_SECRET;
 
@@ -12,6 +13,9 @@ const { NAVER_CLIENT_ID, NAVER_CLIENT_SECRET } = process.env;
 const NAVER_BOOK_URL =
   "https://openapi.naver.com/v1/search/book.json";
 
+// 만약 client mode 로 사용하는 컴포넌트, 함수모듈등에는
+// 함수의 시작부분에 async 를 절대 사용하면 안된다.
+
 export const getNaverBooks = async (search) => {
   const fetchOption = {
     nethod: "GET",
@@ -21,9 +25,12 @@ export const getNaverBooks = async (search) => {
     },
   };
   const response = await fetch(
-    `${NAVER_BOOK_URL}?query=${search}`,
+    `${NAVER_BOOK_URL}?query=${search || "java"}`,
     fetchOption
   );
   const books = await response.json();
+  // naver_api.js 는 server 모듈이다
+  // 이 모듈에서 console.log() 를 사용하면 서버 consol에 출력된다.
+  // console.log("BOOKS", books);
   return books.items;
 };
