5ba831432d1d185d86a6d4c9489007d5001ac401
loo919239 <loo919239@naver.com>
2024-03-14 09:18:09 +0900

    texty

---
+++ nextjs-book-102/src/comps/BookMain.js
@@ -0,0 +1,28 @@
+"use client";
+import { useState } from "react";
+import BookSearch from "./BookSearch";
+import BookList from "./BookList";
+import BookDetail from "./BookDetail";
+import "./BookMain.css";
+
+const BookMain = () => {
+  const [search, setSearch] = useState("");
+  const [isbn, setIsbn] = useState("");
+  return (
+    <article className="book main">
+      <aside className="book list">
+        <BookSearch search={search} setSearch={setSearch} />
+        <BookList search={search} setIsbn={setIsbn} />
+      </aside>
+      <aside className="book detail">
+        {/* 
+        조건부 Rendering 
+        isbn state 값이 "" 또는 null 등이 아니면 BookDetail 을
+        화면에 표현하라
+        */}
+        {isbn && <BookDetail isbn={isbn} />}
+      </aside>
+    </article>
+  );
+};
+export default BookMain;
